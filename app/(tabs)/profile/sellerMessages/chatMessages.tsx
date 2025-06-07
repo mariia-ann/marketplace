66863c8f14e9ab5e12866536874a9cbd64127ae7
Solomiia Kornaha <solomiya.20004@gmail.com>
2025-06-07 21:48:10 +0300

    add sellerMessages

---
+++ app/(tabs)/profile/sellerMessages/chatMessages.tsx
@@ -0,0 +1,18 @@
+import Messages from '@/app-example/components/ui/profile/sellerMessages/Messages';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+
+export default function chatMessages() {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Messages />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+  },
+})  
