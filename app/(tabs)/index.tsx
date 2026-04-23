8dfe54a11134055c107517117ff1f6430346c67d
Solomiia Kornaha <solomiya.20004@gmail.com>
2026-04-23 21:52:51 +0300

    add update favorites page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,15 +4,18 @@ import SvgIcons from '@/src/components/common/SvgIcons/SvgIcons';
 import ItemCard from '@/src/components/ui/home_page/ItemCard';
 import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Colors from '@/constants/Colors';
-import bagpic from "../../assets/images/bagpic.png";
-import whitedress from "../../assets/images/productInCatalog/whitedress.png";
-import toaster from "../../assets/images/productInCatalog/toaster.png";
-import shoes from "../../assets/images/productInCatalog/shoes.png";
-import shoesgold from "../../assets/images/productInCatalog/shoesgold.png";
-import reddress from "../../assets/images/productInCatalog/reddress.png";
+import bagpic from '../../assets/images/bagpic.png';
+import whitedress from '../../assets/images/productInCatalog/whitedress.png';
+import toaster from '../../assets/images/productInCatalog/toaster.png';
+import shoes from '../../assets/images/productInCatalog/shoes.png';
+import shoesgold from '../../assets/images/productInCatalog/shoesgold.png';
+import reddress from '../../assets/images/productInCatalog/reddress.png';
 
 import { useState } from 'react';
 import BannerCarousel from '@/src/components/ui/home_page/carousel/BannerCarousel';
+import { useFavoritesStore } from '@/src/state/useFavoritesStore';
+import PopUpFavorites from '@/src/components/ui/favorites_page/PopUpFavorites';
+import { router } from 'expo-router';
 
 export interface Product {
   id: number;
@@ -26,52 +29,64 @@ export interface Product {
 }
 
 export default function HomeScreen() {
-
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
-  const [selectedPillIndex, setSelectedPillIndex] = useState<number | null>(null);
+  const [selectedPillIndex, setSelectedPillIndex] = useState<number | null>(
+    null,
+  );
   const arrowRightIconSize: number = 30;
+  const { toggleFavorite } = useFavoritesStore();
+
+  const [isPopUpVisible, setIsPopUpVisible] = useState(false);
+  const [popUpTitle, setPopUpTitle] = useState<string>('');
+  const [popUpButtonTitle, setPopUpButtonTitle] = useState<string>('');
 
   const categoryData = [
     {
-      name: "Одяг",
+      name: 'Одяг',
       icon: CUSTOM_ICON_REF.CoatHangar,
     },
     {
-      name: "Взуття",
+      name: 'Взуття',
       icon: CUSTOM_ICON_REF.Sneaker,
     },
     {
-      name: "Сумки",
+      name: 'Сумки',
       icon: CUSTOM_ICON_REF.BackPack,
     },
     {
-      name: "Аксесуари",
+      name: 'Аксесуари',
       icon: CUSTOM_ICON_REF.Eyeglasses,
     },
     {
-      name: "Косметика",
+      name: 'Косметика',
       icon: CUSTOM_ICON_REF.DiamondLogo,
     },
     {
-      name: "Дім",
+      name: 'Дім',
       icon: CUSTOM_ICON_REF.Armchair,
     },
     {
-      name: "Електорніка",
+      name: 'Електорніка',
       icon: CUSTOM_ICON_REF.Robot,
     },
     {
-      name: "Сервіс",
+      name: 'Сервіс',
       icon: CUSTOM_ICON_REF.PeopleLogo,
     },
   ];
 
-  const pillsOptions = ["Усе", "Популярні", "Нові надходження", "Знижки", "Рекомендовані"];
+  const pillsOptions = [
+    'Усе',
+    'Популярні',
+    'Нові надходження',
+    'Знижки',
+    'Рекомендовані',
+  ];
 
   const [products, setProducts] = useState<Product[]>([
     {
       id: 1,
-      title: "Жіночі черевики на підборах з натураль…",
+      title: 'Жіночі черевики на підборах з натураль…',
       price: 3600,
       oldPrice: 4200,
       rating: 4.5,
@@ -80,7 +95,7 @@ export default function HomeScreen() {
     },
     {
       id: 2,
-      title: "Чоботи Марсала жіночі осінні",
+      title: 'Чоботи Марсала жіночі осінні',
       price: 4199,
       rating: 4.5,
       image: shoesgold,
@@ -88,7 +103,7 @@ export default function HomeScreen() {
     },
     {
       id: 3,
-      title: "Жіноче плаття лляне літнє “Summer Bre…”",
+      title: 'Жіноче плаття лляне літнє “Summer Bre…”',
       price: 6290,
       rating: 5.0,
       image: reddress,
@@ -155,35 +170,98 @@ export default function HomeScreen() {
       } else {
         setSelectedIndex(indexSelected);
       }
-    }
+    };
 
-    const selectedBorderColor: string = isSelected ? Colors.softPurple : Colors.white;
+    const selectedBorderColor: string = isSelected
+      ? Colors.softPurple
+      : Colors.white;
 
     return (
-      <Pressable key={index} style={styles.categoryPressable} onPress={() => handleSelectedIndex(index)}>
-        <View style={[styles.categoryCircle, { width: circleSize, height: circleSize, borderRadius: circleSize / 2, borderColor: selectedBorderColor }]}>
-          <SvgIcons name={item.icon} baseStyle={[styles.categoryIcon, { width: iconSize, height: iconSize }]} />
+      <Pressable
+        key={index}
+        style={styles.categoryPressable}
+        onPress={() => handleSelectedIndex(index)}
+      >
+        <View
+          style={[
+            styles.categoryCircle,
+            {
+              width: circleSize,
+              height: circleSize,
+              borderRadius: circleSize / 2,
+              borderColor: selectedBorderColor,
+            },
+          ]}
+        >
+          <SvgIcons
+            name={item.icon}
+            baseStyle={[
+              styles.categoryIcon,
+              { width: iconSize, height: iconSize },
+            ]}
+          />
         </View>
         <Text style={styles.categoryLabel}>{item.name}</Text>
       </Pressable>
-    )
-  }
+    );
+  };
+
+  const showAddedToFavoritesPopUp = () => {
+    setPopUpTitle('Товар додано в обрані');
+    setPopUpButtonTitle('Перейти');
+    setIsPopUpVisible(true);
+    setTimeout(() => {
+      setIsPopUpVisible(false);
+    }, 2500);
+  };
 
   const handleSetWishlist = (index: number, isRecommended: boolean) => {
-    if(isRecommended){
+    if (isRecommended) {
       const updatedProducts = [...recommendedProducts];
-      updatedProducts[index].isFavorite = !updatedProducts[index].isFavorite;
-      // Since products is a constant array, we would typically use a state to manage it.
-      // For demonstration, we are just updating the local copy here.
+      const product = updatedProducts[index];
+      const newIsFavorite = !product.isFavorite;
+      updatedProducts[index] = { ...product, isFavorite: newIsFavorite };
       setRecommendedProducts(updatedProducts);
+
+      toggleFavorite(
+        {
+          id: product.id,
+          title: product.title,
+          price: product.price,
+          oldPrice: product.oldPrice,
+          rating: product.rating,
+          image: product.image,
+        },
+        newIsFavorite,
+      );
+
+      if (newIsFavorite) {
+        showAddedToFavoritesPopUp();
+      }
     } else {
       const updatedProducts = [...products];
-      updatedProducts[index].isFavorite = !updatedProducts[index].isFavorite;
-      // Since products is a constant array, we would typically use a state to manage it.
-      // For demonstration, we are just updating the local copy here.
+      const product = updatedProducts[index];
+      const newIsFavorite = !product.isFavorite;
+      updatedProducts[index] = { ...product, isFavorite: newIsFavorite };
       setProducts(updatedProducts);
+
+      toggleFavorite(
+        {
+          id: product.id,
+          title: product.title,
+          price: product.price,
+          oldPrice: product.oldPrice,
+          rating: product.rating,
+          image: product.image,
+        },
+        newIsFavorite,
+      );
+
+      if (newIsFavorite) {
+        showAddedToFavoritesPopUp();
+      }
     }
-  }
+  };
 
   const renderOptionPills = (pill: any, index: number) => {
     const isSelected: boolean = index === selectedPillIndex;
@@ -193,56 +271,104 @@ export default function HomeScreen() {
       } else {
         setSelectedPillIndex(indexSelected);
       }
-    }
-    const pillTextColor: string = isSelected ? Colors.softPurple : Colors.blackMain;
+    };
+    const pillTextColor: string = isSelected
+      ? Colors.softPurple
+      : Colors.blackMain;
     return (
-      <Pressable key={index} style={styles.pillPressable} onPress={() => handleSelectedPillIndex(index)}>
+      <Pressable
+        key={index}
+        style={styles.pillPressable}
+        onPress={() => handleSelectedPillIndex(index)}
+      >
         <Text style={[styles.pillText, { color: pillTextColor }]}>{pill}</Text>
       </Pressable>
-    )
-  }
+    );
+  };
 
   const renderproducts = (item: Product, index: number) => {
     return (
-      <ItemCard key={index} itemName={item.title} addedTowishlist={item.isFavorite} rating={item.rating} handleSetWishlist={()=>handleSetWishlist(index, false)} imageSrc={item.image} discountedPrice={item.price} mrpPrice={item.oldPrice ? item.oldPrice : undefined} />
-    )
-  }
+      <ItemCard
+        key={index}
+        itemName={item.title}
+        addedTowishlist={item.isFavorite}
+        rating={item.rating}
+        handleSetWishlist={() => handleSetWishlist(index, false)}
+        imageSrc={item.image}
+        discountedPrice={item.price}
+        mrpPrice={item.oldPrice ? item.oldPrice : undefined}
+      />
+    );
+  };
 
   const renderRecommendedProducts = (item: Product, index: number) => {
     return (
-      <ItemCard key={index} itemName={item.title} addedTowishlist={item.isFavorite} rating={item.rating} handleSetWishlist={()=>handleSetWishlist(index, true)} imageSrc={item.image} discountedPrice={item.price} mrpPrice={item.oldPrice ? item.oldPrice : undefined} />
-    )
-  }
+      <ItemCard
+        key={index}
+        itemName={item.title}
+        addedTowishlist={item.isFavorite}
+        rating={item.rating}
+        handleSetWishlist={() => handleSetWishlist(index, true)}
+        imageSrc={item.image}
+        discountedPrice={item.price}
+        mrpPrice={item.oldPrice ? item.oldPrice : undefined}
+      />
+    );
+  };
 
   return (
-    <ScrollView style={styles.container}>
-      <MarketPlaceHeader showSearchBar  />
-      <BannerCarousel baseStyle={styles.bannerCarousel} containerHorizontalPadding={styles.container.padding as number} />
-      <View style={styles.sectionHeaderRow}>
-        <Text style={[styles.categoriesText, styles.sectionHeaderTitle]}>Категорії</Text>
-        <View style={styles.sectionHeaderRight}>
-          <Text style={styles.sectionHeaderLinkText}>Усі</Text>
-          <SvgIcons name={CUSTOM_ICON_REF.Arrowright} baseStyle={styles.sectionHeaderLinkIcon} />
+    <>
+      <ScrollView style={styles.container}>
+        <MarketPlaceHeader showSearchBar />
+        <BannerCarousel
+          baseStyle={styles.bannerCarousel}
+          containerHorizontalPadding={styles.container.padding as number}
+        />
+        <View style={styles.sectionHeaderRow}>
+          <Text style={[styles.categoriesText, styles.sectionHeaderTitle]}>
+            Категорії
+          </Text>
+          <View style={styles.sectionHeaderRight}>
+            <Text style={styles.sectionHeaderLinkText}>Усі</Text>
+            <SvgIcons
+              name={CUSTOM_ICON_REF.Arrowright}
+              baseStyle={styles.sectionHeaderLinkIcon}
+            />
+          </View>
         </View>
-      </View>
-      <ScrollView horizontal style={styles.categoriesScroll}>{categoryData.map(renderCategorydata)}</ScrollView>
-      <View style={styles.rowSpacer} />
-      <View style={styles.forYouHeaderRow}>
-        <Text style={styles.forYouTitle}>Для Вас</Text>
-        <SvgIcons name={CUSTOM_ICON_REF.Arrowright} baseStyle={[styles.forYouArrowIcon, { width: arrowRightIconSize, height: arrowRightIconSize }]} />
-      </View>
-      <View style={styles.recommendedGrid}>
-        {recommendedProducts.map(renderRecommendedProducts)}
-        {/* <ItemCard itemName='Чоботи Марсала жіночі осінні ' addedTowishlist rating={4.5} handleSetWishlist={handleSetWishlist} imageSrc={bagpic} discountedPrice={3000} mrpPrice={4199} /> */}
-      </View>
-      <ScrollView horizontal style={styles.pillsScroll}>
-        {pillsOptions.map(renderOptionPills)}
+        <ScrollView horizontal style={styles.categoriesScroll}>
+          {categoryData.map(renderCategorydata)}
+        </ScrollView>
+        <View style={styles.rowSpacer} />
+        <View style={styles.forYouHeaderRow}>
+          <Text style={styles.forYouTitle}>Для Вас</Text>
+          <SvgIcons
+            name={CUSTOM_ICON_REF.Arrowright}
+            baseStyle={[
+              styles.forYouArrowIcon,
+              { width: arrowRightIconSize, height: arrowRightIconSize },
+            ]}
+          />
+        </View>
+        <View style={styles.recommendedGrid}>
+          {recommendedProducts.map(renderRecommendedProducts)}
+        </View>
+        <ScrollView horizontal style={styles.pillsScroll}>
+          {pillsOptions.map(renderOptionPills)}
+        </ScrollView>
+        <View style={styles.productsGrid}>{products.map(renderproducts)}</View>
       </ScrollView>
-      <View style={styles.productsGrid}>
-        {products.map(renderproducts)}
-        {/* <ItemCard itemName='Чоботи Марсала жіночі осінні ' addedTowishlist rating={4.5} handleSetWishlist={handleSetWishlist} imageSrc={bagpic} discountedPrice={3000} mrpPrice={4199} /> */}
-      </View>
-    </ScrollView>
+
+      <PopUpFavorites
+        visible={isPopUpVisible}
+        titlePopUp={popUpTitle}
+        titleButton={popUpButtonTitle}
+        onPressButton={() => {
+          setIsPopUpVisible(false);
+          router.push('/(tabs)/favorite');
+        }}
+      />
+    </>
   );
 }
 
@@ -261,21 +387,21 @@ const styles = StyleSheet.create({
     color: Colors.blackMain,
   },
   sectionHeaderRow: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     padding: 10,
     paddingRight: 0,
     paddingLeft: 0,
   },
   sectionHeaderTitle: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   sectionHeaderRight: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   sectionHeaderLinkText: {
     fontSize: 16,
@@ -286,65 +412,65 @@ const styles = StyleSheet.create({
     height: 25,
   },
   categoriesScroll: {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "nowrap",
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
     paddingBottom: 20,
   },
   rowSpacer: {
-    display: "flex",
-    flexDirection: "row",
+    display: 'flex',
+    flexDirection: 'row',
   },
   forYouHeaderRow: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   forYouTitle: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     color: Colors.blackMain,
   },
   forYouArrowIcon: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     color: Colors.blackMain,
   },
   recommendedGrid: {
-    display: "flex",
-    flexWrap: "wrap",
-    flexDirection: "row",
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
     paddingTop: 20,
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
   },
   pillsScroll: {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "nowrap",
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
   },
   productsGrid: {
-    display: "flex",
-    flexWrap: "wrap",
-    flexDirection: "row",
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
     paddingTop: 20,
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
     paddingBottom: 30,
   },
   categoryPressable: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
     marginRight: 15,
     marginBottom: 15,
     padding: 8,
   },
   categoryCircle: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: Colors.white,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: { width: 0, height: 6 },
     shadowOpacity: 0.15,
     shadowRadius: 12,
@@ -362,6 +488,6 @@ const styles = StyleSheet.create({
   },
   pillText: {
     fontSize: 16,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
 });
